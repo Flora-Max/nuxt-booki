7098bbd239bb605b70d6c7727b242c57f64e7898
Flora-Max <82108562+Flora-Max@users.noreply.github.com>
2022-03-31 16:49:04 +0200

    feat: filters components

---
+++ packages/frontend/src/components/Filters.vue
@@ -0,0 +1,43 @@
+<template>
+  <li>
+      <a
+        @click.prevent="emitFilter"
+        href="#"
+        :class="[
+          'filters__list__item',
+          { 'filters__list__item--selected': selected }
+        ]"
+        title="Economique"
+      >
+        <span class="filters__list__item__icon">
+          <i  :class="filtre.icon"></i>
+        </span>
+
+        <span class="filters__list__item__text">{{ filtre.name }}</span>
+      </a>
+    </li>
+
+</template>
+
+<script>
+export default {
+  name: 'Filters',
+  props: {
+    filtre: {
+      type: Object,
+      required: true,
+    },
+    selected: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  methods: {
+    emitFilter () {
+      this.$emit('change', this.filtre.value)
+    },
+  }
+
+}
+</script>
---
+++ packages/frontend/src/components/FiltersList.vue
@@ -0,0 +1,41 @@
+<template >
+  <ul class="filters__list">
+    <filtres
+      v-for="(item, i) in items"
+      @change="$emit('change', $event)"
+      :key="i"
+      :filtre="item"
+      :selected="selected === item.value"
+    /> <!-- On rajoute un index dans le item au cas ou 2 noms pareils-->
+  </ul>
+
+</template>
+
+<script>
+export default {
+  name: 'FilterList',
+  model: {  // model appliqué ds index  // evenement= pour passer des infos aux parents; props= pour passer des infos aux enfants
+    prop: 'selected',
+    event: 'change'
+  },
+  props: {
+    selected: {
+      type: [ String, null ],
+      default: null
+    }
+  },
+  data () {
+    return {
+      items: [
+        {name:'Economique', value: 'economic', icon: 'fas fa-money-bill-wave'},
+        {name: 'Familial', value: 'family', icon: 'fas fa-child'},
+        {name: 'Romantique', value: 'romantic', icon: 'fas fa-heart'},
+        {name: 'Animaux autorisés', value: 'pets', icon: 'fas fa-dog'}
+      ],
+    }
+  },
+
+
+
+}
+</script>
