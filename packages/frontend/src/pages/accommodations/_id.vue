fdd48ffac30c237d0f0687e11f22cb68b55789b7
Flora Boularan <flora.b@gmail.com>
2022-04-11 17:10:37 +0200

    feat: add accommodations_id.vue page

---
+++ packages/frontend/src/pages/accommodations/_id.vue
@@ -0,0 +1,49 @@
+<template>
+    <article>
+        <div class="cards-list__item__cover-wrapper">
+          <picture class="cards-list__item__cover">
+            <source media="(min-width: 992px)" :srcset="content.imageLarge">
+            <source media="(min-width: 768px)" :srcset="content.imageMedium">
+            <img :src="content.imageSmall" alt="Aperçu de l'élément" width="250">
+          </picture>
+        </div>
+
+        <div class="cards-list__item__content">
+            <header class="cards-list__item__header">
+                <h3 class="cards-list__item__title">{{ content.name }} {{ content.id }}</h3>
+            </header>
+            <p>
+                <span>{{ content.description }}</span>
+            </p>
+            <p>
+                <span>{{ content.city }}</span>
+            </p>
+            <p>
+               <span>Nuit à partir de {{ content.price }}€</span>
+            </p>
+
+    <!--
+          <aside v-if="content.note !== undefined" class="cards-list__item__stars" :data-stars="accommodations.note">Note de {{ content.note }} sur 5</aside> -->
+
+          <nuxt-link :to="{ name: 'reservations-id' }">Reserver</nuxt-link>
+        </div>
+      </article>
+</template>
+
+<script>
+export default {
+    name: 'InformationsPage',
+
+ data() {
+    return{
+        content: {},
+    }
+ },
+
+    async mounted () {
+       
+    //requete vers l'affichage d'un élément hébergement de notre bdd
+    this.content = await this.$axios.$get(`/display/${this.$route.params.id}`)
+    },
+}
+</script>
