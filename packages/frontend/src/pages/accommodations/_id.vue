285f14ba8f1121208561245c9cd1c4d621338669
Flora Boularan <flora.b@gmail.com>
2022-04-13 11:54:36 +0200

    FEAT routing - reservationForm

--- packages/frontend/src/pages/accommodations/_id.vue
+++ packages/frontend/src/pages/accommodations/_id.vue
@@ -1,49 +1,50 @@
 <template>
-    <article>
-        <div class="cards-list__item__cover-wrapper">
-          <picture class="cards-list__item__cover">
-            <source media="(min-width: 992px)" :srcset="content.imageLarge">
-            <source media="(min-width: 768px)" :srcset="content.imageMedium">
-            <img :src="content.imageSmall" alt="Aperçu de l'élément" width="250">
-          </picture>
-        </div>
+  <article>
+    <div class="cards-list__item__cover-wrapper">
+      <picture class="cards-list__item__cover">
+        <source media="(min-width: 992px)" :srcset="content.imageLarge" />
+        <source media="(min-width: 768px)" :srcset="content.imageMedium" />
+        <img :src="content.imageSmall" alt="Aperçu de l'élément" width="250" />
+      </picture>
+    </div>
 
-        <div class="cards-list__item__content">
-            <header class="cards-list__item__header">
-                <h3 class="cards-list__item__title">{{ content.name }} {{ content.id }}</h3>
-            </header>
-            <p>
-                <span>{{ content.description }}</span>
-            </p>
-            <p>
-                <span>{{ content.city }}</span>
-            </p>
-            <p>
-               <span>Nuit à partir de {{ content.price }}€</span>
-            </p>
+    <div class="cards-list__item__content">
+      <header class="cards-list__item__header">
+        <h3 class="cards-list__item__title">{{ content.name }}</h3>
+      </header>
+      <p>
+        <span><strong>Description: </strong>{{ content.description }}</span>
+      </p>
+      <p>
+        <span><strong>Ville: </strong>{{ content.city }}</span>
+      </p>
+      <p>
+        <span><strong>Nuit à partir de: </strong>{{ content.price }}€</span>
+      </p>
 
-    <!--
+      <!--
           <aside v-if="content.note !== undefined" class="cards-list__item__stars" :data-stars="accommodations.note">Note de {{ content.note }} sur 5</aside> -->
 
-          <nuxt-link :to="{ name: 'reservations-id' }">Reserver</nuxt-link>
-        </div>
-      </article>
+      <nuxt-link :to="{ name: 'reservations-id', params: { id: content.id } }"
+        >Reserver</nuxt-link
+      >
+    </div>
+  </article>
 </template>
 
 <script>
 export default {
-    name: 'InformationsPage',
+  name: "InformationsPage",
 
- data() {
-    return{
-        content: {},
-    }
- },
+  data() {
+    return {
+      content: {},
+    };
+  },
 
-    async mounted () {
-       
+  async mounted() {
     //requete vers l'affichage d'un élément hébergement de notre bdd
-    this.content = await this.$axios.$get(`/display/${this.$route.params.id}`)
-    },
-}
+    this.content = await this.$axios.$get(`/display/${this.$route.params.id}`);
+  },
+};
 </script>
--- packages/frontend/src/pages/reservations/_id.vue
+++ packages/frontend/src/pages/reservations/_id.vue
@@ -9,40 +9,51 @@
       </section>
 
       <section>
-        <form id="formReservation">
-              <div>
-                  <label for="username">Username</label>
-                  <input type="text" id="name" v-model="username">
-              </div>
-                <div>
-                  <label for="email">Email</label>
-                  <input type="email" id="email" v-model="email">
-                </div>
-                <div>
-                  <label for="password">Mot de passe</label>
-                  <input type="password" id="password" v-model="password">
-                </div>
-                 <div>
-                  <label for="quantityPeople">Nombre de personne:</label>
-                  <input type="text" id="quantityPeople" v-model="quantityPeople">
-              </div>
-               <div>
-                  <label for="quantityNight">Nombre de nuit</label>
-                  <input type="text" id="quantityNight" v-model="quantityNight">
-              </div>
-               <div>
-                  <label for="firstNightDate">Date de début de séjour:</label>
-                  <input type="date" id="firstNightDate" v-model="firstNightDate">
-              </div>
-              <div>
-                <label for="creationDate">Date de création de la réservation:</label>
-                <input type="date" id="creationDate" v-model="creationDate">
-              </div>
-              
-              <div>
-                  <button type="submit">Envoyer</button>
-              </div>
-          </form>
+        <form @submit.prevent="submit" id="formReservation">
+          <div>
+            <label for="hebergementName">Nom de l'établissement:</label>
+            <input
+              type="text"
+              id="hebergementName"
+              readonly
+              :value="content.name"
+            />
+          </div>
+          <div>
+            <label for="quantityPeople">Nombre de personne:</label>
+            <input
+              type="number"
+              id="quantityPeople"
+              v-model.number="form.quantityPeople"
+            />
+          </div>
+          <div>
+            <label for="quantityNight">Nombre de nuit</label>
+            <input
+              type="number"
+              id="quantityNight"
+              v-model.number="form.quantityNight"
+            />
+          </div>
+          <div>
+            <label for="firstNightDate">Date de début de séjour:</label>
+            <input
+              type="date"
+              id="firstNightDate"
+              v-model="form.firstNightDate"
+            />
+          </div>
+          <div>
+            <label for="creationDate"
+              >Date de création de la réservation:</label
+            >
+            <input type="date" id="creationDate" v-model="form.creationDate" />
+          </div>
+
+          <div>
+            <button type="submit">Envoyer</button>
+          </div>
+        </form>
       </section>
     </main>
   </div>
@@ -50,23 +61,45 @@
 
 <script>
 export default {
-    name: 'PageReservation',
+  name: "PageReservation",
 
-    data(){
-      return{
-        item: {},
-      }
-    },
-    
-
-    async mounted () {
-      //requete pour envoyer données de formulaire de notre réservation vers la bdd
-      await this.$axios.$post('http://localhost:8000/reservationForm', 
-      { username, email, username, password, quantityPeople, quantityNight, firstNightDate, creationDate })
-      .then((res) => {
-      console.log(res)
-      return this.$router.redirect({ name: 'index' })
-        })
+  data() {
+    return {
+      content: {},
+      form: {
+        hebergementId: null,
+        quantityPeople: 0,
+        quantityNight: 0,
+        firstNightDate: null,
+        creationDate: null,
       },
-  }
+    };
+  },
+
+  async mounted() {
+    //requete vers l'affichage d'un élément hébergement de notre bdd
+    this.content = await this.$axios.$get(`/display/${this.$route.params.id}`);
+    this.form.hebergementId = this.content.id
+  },
+
+  methods: {
+    submit() {
+      console.log(this.form);
+      //dataForm = JSON.stringify({form})
+      //requete pour envoyer données de formulaire de notre réservation vers la bdd
+      //return this.$axios.$post('/reservationForm', this.form)
+      //.then((res) => {
+      // console.log(res)
+      // return this.$router.redirect({ name: 'index' })
+      this.$axios
+        .$post(`/reservationForm/${this.content.id}`, this.form)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+    },
+  },
+};
 </script>
